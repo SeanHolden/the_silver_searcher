bde9fd649f0d9fbed8d30d0bb6e605af41cd55bc
Sean Holden <sean.holden@onthebeach.co.uk>
2017-02-14 18:58:50 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# the_silver_searcher
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: the_silver_searcher
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
